6d95c852afd3ceb22e613be688b7c490ac7bdaf9
Siddharth Yadav <siddharthyadav92675@gmail.com>
2025-12-02 22:51:25 +0530

    Add files via upload

---
+++ 242.java
@@ -0,0 +1,32 @@
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0){ 
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
